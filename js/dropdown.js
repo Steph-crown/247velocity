764c72842106349cf9f3d0b05f1cda2e1a9ba0b9
steph-crown <emmanuelstephen024@gmail.com>
2021-12-19 21:30:52 +0100

    changed video and added dropdown

---
+++ js/dropdown.js
@@ -0,0 +1,13 @@
+const menuIcon = document.querySelector("div.menu-icon");
+const closeIcon = document.querySelector("p.times");
+const dropdown = document.querySelector("div.dropdown");
+
+const dropdownContent = document.querySelector("div.dropdown-content");
+
+menuIcon.addEventListener("click", (e) => {
+    dropdownContent.style.left = "0";
+});
+
+closeIcon.addEventListener("click", (e) => {
+    dropdownContent.style.left = "100vw";
+});
